76938d7523c096c923dd266165369e14484c5a5b
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-25 16:35:45 -0300

    feat: BlogPost model implemented

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,31 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreingKey: true
+    }
+  },
+  {
+    timestamps: false
+  });
+  
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreingKey: 'userId',
+      as: 'users'
+    })
+  }
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
