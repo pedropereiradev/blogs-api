251b956340603bed4e9dcecdfdc5267e87b1cea9
Pedro Pereira <pedro.2108@hotmail.com>
2022-08-25 16:35:10 -0300

    feat: User model implemented

---
+++ src/database/models/user.js
@@ -0,0 +1,27 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+  {
+    timestamps: false
+  });
+  
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreingKey: 'userId',
+      as: 'blogPosts'
+    });
+  };
+
+  return User;
+};
+
+module.exports = User;
